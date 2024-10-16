753e1458759322c493050b8211b642c08c1d8b3a
Touseef Jutt <touxeef.jutt@outlook.com>
2024-10-16 19:12:55 +0500

    first commit

---
+++ components/Home/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Image from "next/image";
+import logo from '/public/images/logo.png'; // Direct path from the public folder
+
+export default function index() {
+  return (
+    <>
+      <header>
+        <div className="top-bar flex justify-between">
+          {/* <Image alt="logo"> </Image> */}
+          <Image src={logo} alt="logo" width={129} height={47} />
+          <ul className='flex items-center text-white'>
+            <li>
+            <h5>
+              Top Selling
+            </h5>
+            </li>
+            <li>
+              Top Activities
+            </li>
+            <li>
+              Burj Khalifa
+            </li>
+            <li>
+              Desert Safari
+            </li>
+            <li>
+              |
+            </li>
+            <li>
+              Login
+            </li>
+          </ul>
+        </div>
+      </header>
+
+      <div>index</div>
+    </>
+  );
+}
